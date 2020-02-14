49cc38d0cd80636f9d18065d06aab2d377ca9a0e
Eryk Rachwal <erykrachwal@yahoo.com>
2020-02-14 15:28:42 +0000

    initial CLI setup with greeting

---
+++ alpine_pro/lib/cli.rb
@@ -0,0 +1,17 @@
+require_relative 'mountain.rb'
+require_relative 'scraper.rb'
+require 'colorize'
+require 'readline'
+
+module AlpinePro
+  class CLI 
+    
+    #Program begins by running the list_altitude method 
+  def start 
+    list_altitude
+  end 
+  
+    #Greeting 
+  def list_altitude
+    puts "Hello Explorer! Please indicate your desired summit altitude.".black.bold
+    puts "\nSelect an altitude from 1 - 4".blue.bold 
