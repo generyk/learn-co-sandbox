cac5fe51ada2ca8679faf130782898bcaf7b56c4
Eryk Rachwal <erykrachwal@yahoo.com>
2020-02-14 19:54:45 +0000

    added method to scraper

--- alpine_pro/lib/scraper.rb
+++ alpine_pro/lib/scraper.rb
@@ -1,4 +1,11 @@
 class AlpinePro::Scraper
   
-  def self.scrape_data
+  def self.list_mountain_names
+    self.scrape_mountains
+  end 
+  
+  def self.scrape_mountains
+    doc = Nokogiri::HTML(open('https://www.thegentlemansjournal.com/article/top-10-mountains-to-climb-around-the-world/'))
+    
+        
     
