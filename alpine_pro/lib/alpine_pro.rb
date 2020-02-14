0302f5012377956b1097d7ad99934e77a0792960
Eryk Rachwal <erykrachwal@yahoo.com>
2020-02-14 18:34:53 +0000

    visual changes to cli

--- alpine_pro/lib/alpine_pro.rb
+++ alpine_pro/lib/alpine_pro.rb
@@ -1,4 +1,8 @@
 require "alpine_pro/version"
+require "alpine_pro/cli"
+require "alpine_pro/scraper"
+require 'open-uri'
+require 'nokogiri'
 
 module AlpinePro
   class Error < StandardError; end
--- alpine_pro/lib/cli.rb
+++ alpine_pro/lib/cli.rb
@@ -7,11 +7,16 @@ module AlpinePro
   class CLI 
     
     #Program begins by running the list_altitude method 
-  def start 
-    list_altitude
-  end 
+    def start 
+      list_altitude
+    end 
   
     #Greeting 
-  def list_altitude
-    puts "Hello Explorer! Please indicate your desired summit altitude.".black.bold
-    puts "\nSelect an altitude from 1 - 4".blue.bold 
+    def list_altitude
+      puts ""
+      puts "Hello Explorer! Please indicate your desired summit altitude.".black.bold
+      puts "\nSelect an altitude from 1 - 4".blue.bold 
+      puts ""
+    end 
+  end 
+end 
